d6d80938b5b1ebf7be63788fcd6a94e6ef97986a
Olgaluiisa <olgaluiisa@hotmail.com>
2022-09-06 21:57:33 -0300

    feat: finished card component

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,1 +1,10 @@
-//{}
+import { Container, Imagem, Name, Description } from "./style";
+export const Card = ({ name, category, img }) => {
+  return (
+    <Container>
+      <Imagem src={img} alt={name} />
+      <Name>{name}</Name>
+      <Description>{category}</Description>
+    </Container>
+  );
+};
--- src/Components/Card/style.js
+++ src/Components/Card/style.js
@@ -1,1 +1,30 @@
-//{}
+import styled from "styled-components";
+
+export const Container = styled.section`
+  width: 230px;
+  height: 280px;
+  padding: 20px 10px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--cinza-1);
+  box-shadow: 3px 3px 3px 0 rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  border-radius: 4px;
+`;
+
+export const Imagem = styled.img`
+  width: 180px;
+  height: 180px;
+  margin: 0 auto 5px;
+`;
+
+export const Name = styled.h3`
+  font-size: 1.2rem;
+  color: var(--branco-1);
+`;
+export const Description = styled.h5`
+  font-size: 0.8rem;
+  font-weight: 400;
+  color: var(--branco-1);
+`;
