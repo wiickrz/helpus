df804399cdf195973c39fc22cc43fcb8c3f4f3f5
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-09-12 23:45:43 -0300

    feat: creat Input Component

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,1 +1,27 @@
-//{}
+import * as S from "./style";
+
+export const TextField = ({
+  label,
+  type,
+  icon: Icon,
+  register,
+  name,
+  error = "",
+  toggleType = () => {},
+  ...props
+}) => {
+  return (
+    <S.Input error={!!error}>
+      <label>{label}</label>
+      <div>
+        <input type={type} {...register(name)} {...props} />
+        {Icon && (
+          <div onClick={toggleType}>
+            <Icon />
+          </div>
+        )}
+        {!!error && <S.Error>{error}</S.Error>}
+      </div>
+    </S.Input>
+  );
+};
--- src/Components/Input/style.js
+++ src/Components/Input/style.js
@@ -1,1 +1,41 @@
-//{}
+import styled from "styled-components";
+
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 1.5rem;
+
+  label {
+    color: var(--branco-1);
+    font-size: 16px;
+  }
+
+  input {
+    max-width: 100%;
+    background-color: #cccccc;
+    border: 2px solid
+      ${(props) => (props.error ? "var(--vermelho-1)" : "#cccccc")};
+    font-size: 18px;
+    padding: 0 10px;
+    font-weight: bold;
+    color: var(--cinza-1);
+    height: 45px;
+    width: 100%;
+
+    &:focus {
+      border: 2px solid
+        ${(props) => (props.error ? "var(--vermelho-1)" : "var(--azul-1)")};
+    }
+
+    &::placeholder {
+      color: var(--cinza-1);
+      font-weight: 400;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  color: var(--vermelho-1);
+  font-size: 14px;
+`;
