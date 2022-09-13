72e718847a60095349f59549987f8b66e89f8aa0
André Silva <69265953+guerreirografico@users.noreply.github.com>
2022-09-13 20:32:47 -0300

    feat: finished contact component

--- src/Components/Contact/style.js
+++ src/Components/Contact/style.js
@@ -2,19 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     background-color: var(--cinza-1);
-    width: 1262px;
-    height: 570px;
+    margin-top: 40px;
+    width: 90%;
+    height: 632px;
     color: var(--branco-1);
     display: flex;
     flex-direction: column;
     align-items: center;
 
+    @media screen and (min-width: 1440px) {
+        font-size: 32px;
+        line-height: 36px;
+        width: 1262px;
+        height: 570px;
+    }
+
     .button {
-        width: 100%;
         display: flex;
         justify-content: flex-end;
-        margin-right: 60px;
-        margin-top: 10px;
+
+        @media screen and (min-width: 1440px) {
+            width: 95%;
+            margin-top: 0px;
+        }
     }
 
     button {
@@ -24,31 +34,77 @@ export const Container = styled.div`
         align-items: center;
         padding: 5px 15px;
     }
+
+    @media screen and (min-width: 900px) {
+        width: 850px;
+    }
+
+    @media screen and (min-width: 1440px) {
+        width: 1262px;
+    }
 `
 
 export const Content = styled.div `
     h2 {
         font-weight: 700;
-        font-size: 32px;
-        line-height: 48px;
+        font-size: 24px;
+        line-height: 36px;
         text-align: center;
-        margin-top: 60px;
+        margin-top: 46px;
     }
 
-    div {
+    .paragrafo {
         font-weight: 400;
         font-size: 24px;
         text-align: center;
+        margin-top: 20px;
+        width: 236px;
+        height: 270px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
         p {
-           margin-top: 60px;
+           margin-top: 30px;
            text-align: center;
-           width: 741px;
+           font-size: 18px;
+           line-height: 25px;
+           width: 236px;
+
+            @media screen and (min-width: 480px) {
+                width: 350px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                font-size: 20px;
+                margin-bottom: 30px;
+            }
+
+            @media screen and (min-width: 750px) {
+                font-size: 22px;
+                width: 550px;
+                line-height: 35px;
+            }
+
+            @media screen and (min-width: 900px) {
+                font-size: 24px;
+                font-wight: 400;
+                width: 750px;
+                height: 180px;
+                line-height: 36px;
+            }
         }
     } 
 `
 export const ImgContent = styled.div `
-   margin-top: 60px;
+    text-align: center;
+    margin-bottom: 50px;
     img {
         margin-left: 20px;
+        margin-top: 50px;
+
+        @media screen and (min-width: 480px) {
+            margin-top: 30px;
+        }
     }
 `
