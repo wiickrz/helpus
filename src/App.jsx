507e1e7bb38ed35d18f7b2218c60daf2284cd6f1
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2022-09-12 23:52:37 -0300

    feat: add ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { GlobalStyle } from "./Styles/GlobalStyle";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
+      <ToastContainer
+        position="top-right"
+        theme="dark"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 };
