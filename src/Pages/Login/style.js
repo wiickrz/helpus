74bbc3abd46e4a51f972ba250f596d4d5c0095a0
wiickrz <will.cs.182@gmail.com>
2022-09-20 21:37:25 -0300

    Feat: Home Page

--- src/Pages/Login/style.js
+++ src/Pages/Login/style.js
@@ -1,6 +1,14 @@
 //{}
 import styled from "styled-components";
 
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  max-height: 100vh;
+`;
+
 export const Container = styled.div`
   display: flex;
   justify-content: center;
